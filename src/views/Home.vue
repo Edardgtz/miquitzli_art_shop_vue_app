d6438e2929bd98660fa624afdf62e46d1d3a4794
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2021-03-08 16:05:49 -0600

    add index to home.vue name and image display properly

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      <br><p><strong>{{ product.name }}</strong></p>
+      <img v-bind:src="product.images" alt="product image">
+    </div>
   </div>
 </template>
 
@@ -8,13 +12,28 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Products!",
+      products: [],
+      serachTerm: "",
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    console.log("in created..");
+    this.productsIndex();
+  },
+  methods: {
+    productsIndex: function () {
+      console.log("In Products Index");
+      axios.get("api/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+  },
 };
 </script>
